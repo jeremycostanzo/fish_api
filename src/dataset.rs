f6d67f5da41c2cc292f57e0ee47abfe22cae71ee
jeremycostanzo <jeremy.costanzo@mines-paristech.fr>
2021-12-26 19:27:40 +0100

    add unit tests

--- src/dataset.rs
+++ src/dataset.rs
@@ -48,3 +48,58 @@ impl FishDataset {
         self.fish_names.pop()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    const TEST_FILE_NAME: &str = "test_rows.csv";
+    const MAX_ITERATIONS: usize = 50;
+    use crate::FishDataset;
+    fn the_file_is_too_long() {
+        panic!(
+            "The test file is too large, it should contain at most {} entries",
+            MAX_ITERATIONS
+        )
+    }
+
+    fn create_dataset() -> FishDataset {
+        let file = std::fs::File::open(TEST_FILE_NAME)
+            .unwrap_or_else(|_| panic!("could not find {}", TEST_FILE_NAME));
+        FishDataset::from_file(&file).expect("incorrect csv format")
+    }
+    #[test]
+    fn read_from_file() {
+        let mut dataset = create_dataset();
+        assert_eq!(Some("Blue sucker".to_owned()), dataset.random_fish());
+    }
+
+    #[test]
+    fn fishes_appear_only_once() {
+        let mut dataset = create_dataset();
+
+        let mut seen_names = std::collections::HashSet::new();
+        for _ in 1..=MAX_ITERATIONS {
+            let name = dataset.random_fish();
+            if let Some(name) = name {
+                assert!(seen_names.insert(name))
+            } else {
+                return;
+            }
+        }
+        the_file_is_too_long();
+    }
+
+    #[test]
+    fn empty_english_names_are_ignored() {
+        let mut dataset = create_dataset();
+
+        for _ in 1..=MAX_ITERATIONS {
+            let name = dataset.random_fish();
+            if let Some(name) = name {
+                assert_ne!(name, "")
+            } else {
+                return;
+            }
+        }
+        the_file_is_too_long();
+    }
+}
